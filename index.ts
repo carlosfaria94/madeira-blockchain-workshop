5f0c7cb0bd0dde246d642d19b19933407e7159b5
CarlosFaria94 <email@carlosfaria.com>
2023-12-01 12:46:14 +0000

    chore: solution

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 require('dotenv').config()
-
 import { LocalAccountSigner } from '@alchemy/aa-core'
-import { Hash, parseAbi } from 'viem'
+import { ECDSAProvider } from '@zerodev/sdk'
+import { Hash, createPublicClient, encodeFunctionData, http, parseAbi } from 'viem'
 import { generatePrivateKey } from 'viem/accounts'
+import { polygonMumbai } from 'viem/chains'
 
 const projectId = process.env.PROJECT_ID!
 const privateKey = process.env.PRIVATE_KEY as Hash
@@ -16,6 +17,40 @@ const contractABI = parseAbi([
   'function balanceOf(address owner) external view returns (uint256 balance)',
 ])
 
-const main = async () => {}
+const publicClient = createPublicClient({
+  chain: polygonMumbai,
+  transport: http(process.env.ALCHEMY_RPC),
+})
+
+const main = async () => {
+  const provider = await ECDSAProvider.init({
+    projectId,
+    owner,
+  })
+
+  const smartContractAddress = await provider.getAddress()
+  console.log('Smart contract contrafactual address:', smartContractAddress)
+
+  const userOp = await provider.sendUserOperation({
+    target: contractAddress,
+    data: encodeFunctionData({
+      abi: contractABI,
+      functionName: 'mint',
+      args: [smartContractAddress],
+    }),
+  })
+
+  console.log('User Operation', userOp)
+
+  await provider.waitForUserOperationTransaction(userOp.hash as Hash)
+
+  const balanceOf = await publicClient.readContract({
+    address: contractAddress,
+    abi: contractABI,
+    functionName: 'balanceOf',
+    args: [smartContractAddress],
+  })
+  console.log(`NFT balance: ${balanceOf}`)
+}
 
 main().then(() => process.exit(0))
